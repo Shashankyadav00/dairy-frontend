dd2d2052cd816c7cf38bc50e9898956b43c0f6a1
SHASHANK YADAV <shashank09072004@gmail.com>
2025-12-04 19:32:05 +0530

    Updated axios baseURL to Render

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.4:8080",
+  baseURL: "https://dairy-attendance.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
